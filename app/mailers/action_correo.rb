b1e5a3d325b925cefc9b55f24051088b7a98e318
percywoeds <percywoeds@gmail.com>
2016-09-09 16:40:21 -0500

    make it better

--- app/mailers/action_correo.rb
+++ app/mailers/action_correo.rb
@@ -1,16 +1,12 @@
 class ActionCorreo < ApplicationMailer
-  	  default from: 'factura-electronica@valuemiperu.com'
+  	  default from: 'factura-electronica@hidrotransp.com'
+
 
 	  def bienvenido_email(invoice)
 		  @invoices=invoice 			
 		   @url  = 'http://www.apple.com'
 		  #attachments["Factura"] = File.read("#{$lcFileName1}")
 		  #attachments['Factura'] = File.read($lcFileName1)
-<<<<<<< HEAD
-=======
-		  email_with_copy = "Factura Enviada <administracion@valuemiperu.com>"	
-		  email_with_name = "Factura Enviada <factura-electronica@valuemiperu.com>"	
->>>>>>> f73fc28c868609a97618127025826cb6b8de093e
 
 		  email_with_name = "Factura Enviada <factura-electronica@hidrotransp.com>"	
 		  email_with_copy = "Administracion <zportal@hidrotransp.com>"	
@@ -18,11 +14,7 @@ class ActionCorreo < ApplicationMailer
 
 		  attachments[$lcFilezip] =  open($lcFile2).read
 
-<<<<<<< HEAD
 		  mail(to: [$lcMail,$lcMail2,$lcMail3], cc: email_with_copy,   bcc:email_with_name, subject: 'Factura Electrónica : '+$lcFileNameIni )
-=======
-		  mail(to: [$lcMail,$lcMail2,$lcMail3], cc: email_with_copy, bcc:email_with_name, subject: 'Factura Electrónica : '+$lcFileNameIni )
->>>>>>> f73fc28c868609a97618127025826cb6b8de093e
 
 
 	  end
