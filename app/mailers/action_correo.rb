d26e719ef6838762f0e60fac0f929aa5b7863955
percywoeds <percywoeds@gmail.com>
2016-09-09 16:58:12 -0500

    vendor compiled assets

--- app/mailers/action_correo.rb
+++ app/mailers/action_correo.rb
@@ -8,7 +8,7 @@ class ActionCorreo < ApplicationMailer
 		  #attachments["Factura"] = File.read("#{$lcFileName1}")
 		  #attachments['Factura'] = File.read($lcFileName1)
 
-		  email_with_name = "Factura Enviada <factura-electronica@hidrotransp.com>"	
+		  email_with_name = "Factura Enviada <factura-electronica@valuemiperu.com>"	
 		  email_with_copy = "Administracion <zportal@hidrotransp.com>"	
 		  attachments[$lcFileName] =  open($lcFileName1).read
 
